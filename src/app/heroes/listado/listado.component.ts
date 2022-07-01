d4d9e8332b4a60bca7be01443708dd89cfb98cdc
Edwin <edwincontri@hotmail.com>
2022-07-01 13:20:28 -0500

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: 'listado.component.html'
+  
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['spiderman','ironman','hulk','thor','capitan america'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void{
+    this.heroeBorrado = this.heroes.shift() || '';    
+  }   
+ 
+}
+
+
+
